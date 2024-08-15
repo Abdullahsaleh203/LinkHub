de97100afba4c14401a22da3a5f21512788b3bfa
Mhamed Gharieb <m.gharieb89@gmail.com>
2024-08-15 13:44:30 +0300

    finishing the setup and start frontend

--- LinkHub/essential/views.py
+++ LinkHub/essential/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return HttpResponse('<h1>Welcome To Your LinkHub Platform</h1>')
+    return render(request, 'index.html')
