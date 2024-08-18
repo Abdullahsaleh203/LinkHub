f5592f3c8c4400def93264a72104b2132c189c7a
abdallah gamal <abdallah.saleh203@yahoo.com>
2024-08-18 22:52:27 +0300

    Update Django settings for JWT authentication and CORS

--- LinkHub/LinkHub/settings.py
+++ LinkHub/LinkHub/settings.py
@@ -61,8 +61,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'djangorestframework',
-    'djangorestframework_simplejwt',
+    'rest_framework',
+    'rest_framework_simplejwt',
     'corsheaders',
     'essential',
 ]
