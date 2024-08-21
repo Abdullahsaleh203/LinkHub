8608ebec8752459a74d0d02ef1388029501e51d4
Abdallah Saleh <abdallah.saleh203@yahoo.com>
2024-08-21 15:21:20 +0300

    Add post module for creating and managing posts

--- LinkHub/LinkHub/urls.py
+++ LinkHub/LinkHub/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+# from django.conf import settings
+# from django.conf.urls.static import static
 
 urlpatterns = [
     path('api/', include('account.urls')),
+    path('api/posts/', include('post.urls')),
     path('admin/', admin.site.urls),
     # path('', include('essential.urls')),
 ]
