1f6e76848073f4d06af24a2e4bf6a91eb49d6e7f
Abdallah Saleh <abdallah.saleh203@yahoo.com>
2024-08-25 15:28:07 +0300

    Add search functionality to API

--- LinkHub/search/api.py
+++ LinkHub/search/api.py
@@ -1,11 +1,20 @@
 from django.http import JsonResponse
 from rest_framework.decorators import  permission_classes, authentication_classes ,api_view
 
+from post.serializers import PostSerializer
+from post.models import Post
+from account.models import User
+from account.serializers import UserSerializer
+
 
 @api_view(['POST'])
 def search(request):
     data = request.data
     query = data['query']
-    print('query',query)
-    return JsonResponse({'query': query})
+    users = User.objects.filter(name__icontains=query)
+    users_serializer = UserSerializer(users, many=True)
+    post= Post.objects.filter(title__icontains=query)
+    post_serializer = PostSerializer(post, many=True)
+    return JsonResponse({'users': users_serializer.data, 'posts': post_serializer.data}, safe=False)
+    # return JsonResponse(users_serializer.data, safe=False)
   
