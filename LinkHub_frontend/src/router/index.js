9760469b63b4f4bd804b8fb9115a0a8f8585206e
Mo Gharieb <m.gharieb89@gmail.com>
2024-08-26 16:00:13 +0300

    fixing static files

--- LinkHub_frontend/src/router/index.js
+++ LinkHub_frontend/src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import SignupView from '../views/SignupView.vue'
+import SigninView from '../views/SigninView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +10,16 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView
+    },
+    {
+      path: '/signup',
+      name: 'signup',
+      component: SignupView
+    },
+    {
+      path: '/signin',
+      name: 'signin',
+      component: SigninView
     },
     {
       path: '/about',
