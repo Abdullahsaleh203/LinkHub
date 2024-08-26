83a418b1e71e041911366eb9b8d94068e9338320
Mo Gharieb <m.gharieb89@gmail.com>
2024-08-26 20:49:32 +0300

    signup preperation

--- LinkHub_frontend/src/views/SignupView.vue
+++ LinkHub_frontend/src/views/SignupView.vue
@@ -17,27 +17,33 @@
 
         <div class="main-right">
             <div class="p-12 bg-white border border-yellow-700 rounded-lg">
-                <form class="space-y-6">
+                <form class="space-y-6" v-on:submit.prevent="submitForm">
                     <div>
                         <label>Name</label><br>
-                        <input type="text" placeholder="Your full name" class="w-full mt-2 py-4 px-6 border border-green-600 rounded-lg">
+                        <input type="text" v-model="form.name" placeholder="Your full name" class="w-full mt-2 py-4 px-6 border border-green-600 rounded-lg">
                     </div>
 
                     <div>
                         <label>E-mail</label><br>
-                        <input type="email" placeholder="Your e-mail address" class="w-full mt-2 py-4 px-6 border border-green-600 rounded-lg">
+                        <input type="email" v-model="form.email" placeholder="Your e-mail address" class="w-full mt-2 py-4 px-6 border border-green-600 rounded-lg">
                     </div>
 
                     <div>
                         <label>Password</label><br>
-                        <input type="password" placeholder="Your password" class="w-full mt-2 py-4 px-6 border border-green-600 rounded-lg">
+                        <input type="password" v-model="form.password1" placeholder="Your password" class="w-full mt-2 py-4 px-6 border border-green-600 rounded-lg">
                     </div>
 
                     <div>
                         <label>Repeat password</label><br>
-                        <input type="password" placeholder="Repeat your password" class="w-full mt-2 py-4 px-6 border border-green-600 rounded-lg">
+                        <input type="password" v-model="form.password2" placeholder="Repeat your password" class="w-full mt-2 py-4 px-6 border border-green-600 rounded-lg">
                     </div>
 
+                    <template v-if="errors.length > 0">
+                        <div class="bg-red-300 text-white rounded-lg p-6">
+                            <p v-for="error in errors" v-bind:key="error">{{ error }}</p>
+                        </div>
+                    </template>
+                    
                     <div>
                         <button class="py-4 px-6 bg-emerald-600 text-white rounded-lg">Sign up</button>
                     </div>
@@ -47,3 +53,78 @@
     </div>
 </template>
 
+
+<script>
+import axios from 'axios'
+
+import { useToastStore } from '@/stores/toast'
+
+export default {
+    setup() {
+        const toastStore = useToastStore()
+
+        return {
+            toastStore
+        }
+    },
+
+    data() {
+        return {
+            form: {
+                email: '',
+                name: '',
+                password1: '',
+                password2: ''
+            },
+            errors: [],
+        }
+    },
+
+    methods: {
+        submitForm() {
+            this.errors = []
+
+            if (this.form.email === '') {
+                this.errors.push('Your e-mail is missing')
+            }
+
+            if (this.form.name === '') {
+                this.errors.push('Your name is missing')
+            }
+
+            if (this.form.password1 === '') {
+                this.errors.push('Your password is missing')
+            }
+
+            if (this.form.password1 !== this.form.password2) {
+                this.errors.push('The password does not match')
+            }
+
+            if (this.errors.length === 0) {
+                axios
+                    .post('/api/signup/', this.form)
+                    .then(response => {
+                        if (response.data.message === 'success') {
+                            this.toastStore.showToast(5000, 'The user is registered. Please activate your account by clicking your email link.', 'bg-pink-500')
+
+                            this.form.email = ''
+                            this.form.name = ''
+                            this.form.password1 = ''
+                            this.form.password2 = ''
+                        } else {
+                            const data = JSON.parse(response.data.message)
+                            for (const key in data){
+                                this.errors.push(data[key][0].message)
+                            }
+
+                            this.toastStore.showToast(5000, 'Something went wrong. Please try again', 'bg-yellow-300')
+                        }
+                    })
+                    .catch(error => {
+                        console.log('error', error)
+                    })
+            }
+        }
+    }
+}
+</script>
